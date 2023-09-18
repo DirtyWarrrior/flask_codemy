e17786b5a01f633c6be32e82d1e30764211f3759
DirtyWarrrior <lorenzfiedel@gmail.com>
2023-09-18 20:53:08 +0200

    first change test

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def base():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
-# Ivalid Server Error
+# Ivalid Server Error thing
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
@@ -32,3 +32,10 @@ def page_not_found(e):
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+# save code on git hub version controle
+
+# git add .
+# git commit -am "what did i change?"
+#
