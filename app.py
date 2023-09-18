1061e654f6f8d2c3fc0258cf7dea5793a9e40353
Lorenz Rockstuhl <lorenzfiedel@gmail.com>
2023-09-18 19:43:39 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, url_for
+
+
+app = Flask(__name__)
+
+
+# user a variable called name localhos:port/test/lorenz will have a resulte
+@app.route("/test/<name>")
+def username(name):
+    return "<h1>How are you {}?????</h1>".format(name)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/base")
+def base():
+    return render_template("base.html")
+
+
+# Create Custom Error Pages
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+# Ivalid Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
